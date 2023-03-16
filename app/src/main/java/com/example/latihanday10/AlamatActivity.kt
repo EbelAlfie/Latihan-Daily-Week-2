26dd2a8b3aa02391ba3e70a7b938da03bba6d62a
EbelAlfie <davis.onde.wei@gmail.com>
2023-03-16 23:56:17 +0700

    final branch one

--- app/src/main/java/com/example/latihanday10/AlamatActivity.kt
+++ app/src/main/java/com/example/latihanday10/AlamatActivity.kt
@@ -2,6 +2,8 @@ package com.example.latihanday10
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.latihanday10.databinding.ActivityAlamatBinding
 import com.example.latihanday10.model.GeneralModel
@@ -22,13 +24,36 @@ class AlamatActivity : AppCompatActivity() {
         dataKecamatan = Utils.getIntentData(this, Utils.KECAMATAN_KEY)
         dataKelurahan = Utils.getIntentData(this, Utils.KELURAHAN_KEY)
 
-        dataProvinsi?.let{binding.tvProvinsi.text = it.nama}
-        dataKota?.let{binding.tvKota.text = it.nama }
-        dataKecamatan?.let{binding.tvKecamatan.text = it.nama }
-        dataKelurahan?.let{binding.tvKelurahan.text = it.nama}
+        if (dataProvinsi == null && dataKota != null &&
+            dataKecamatan == null && dataKelurahan == null) {
+            hideView(binding.tvProvinsi)
+            hideView(binding.tvKota)
+            hideView(binding.tvKecamatan)
+            hideView(binding.tvKelurahan)
+        }else {
+            showView(binding.tvProvinsi)
+            showView(binding.tvKota)
+            showView(binding.tvKecamatan)
+            showView(binding.tvKelurahan)
+
+            binding.btnGantiAlamat.text = getString(R.string.ganti_alamat)
+
+            dataProvinsi?.let{binding.tvProvinsi.text = it.nama}
+            dataKota?.let{binding.tvKota.text = it.nama }
+            dataKecamatan?.let{binding.tvKecamatan.text = it.nama }
+            dataKelurahan?.let{binding.tvKelurahan.text = it.nama}
+        }
 
         binding.btnGantiAlamat.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
         }
+    }
+
+    private fun showView(tv: TextView) {
+        tv.visibility = View.VISIBLE
+    }
+
+    private fun hideView(tv: TextView) {
+        tv.visibility = View.GONE
     }
 }
